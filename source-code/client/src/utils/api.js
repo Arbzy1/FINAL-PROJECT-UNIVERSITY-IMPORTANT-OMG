759de541e2f3ffb98dce11d6a26f03a70c18b85e
Aden Butt <adenreh@gmail.com>
2025-05-05 18:05:15 +0100

    more  vite changes

--- source-code/client/src/utils/api.js
+++ source-code/client/src/utils/api.js
@@ -1,2 +1,2 @@
 // API configuration
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000'; 
+export const API_URL = import.meta.env.VITE_API_URL || 'https://tranquilty-backend-core.onrender.com'; 
